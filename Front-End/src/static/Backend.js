bab21427367ee8f16392a3aaae1182a431c71cdb
Dhruvil <dhruvilsshah@gmail.com>
2020-10-11 17:49:44 -0400

    Refactoring Project

---
+++ Front-End/src/static/Backend.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Home';
+import Signup from './Signup';
+import ProjectDetails from './ProjectDetails';
+import './static/App.css';
+
+class App extends Component {
+  method (params) =>
+{
+    getdatafrombackend(). re}
+    return data
+}
+}
+
+
