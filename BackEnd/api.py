dcb0fc250da84cdf58739bf097f4c9f3bf90e0e0
Dhruvil <dhruvilsshah@gmail.com>
2020-10-13 18:36:00 -0400

    Refactor Backend and add fallback to AWS database.

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -1,69 +1,39 @@
-from flask import Flask, redirect
-from flask import request
-from flask_cors import CORS
-import json
+# ToDo: Remove CORS Dependency
+# ToDo: Restructure ProjectDetails Function: High Complexity
 
-from BackEnd.connection import connection
+from flask import Flask, redirect
+from flask import request, jsonify
+from flask_cors import CORS
+
+import BackEnd.connection as connection
 
 app = Flask(__name__)
 cors = CORS(app)
 
-
 @app.route('/executeAlgo')
 def execute_algo():
     exec(open("algo.py").read())
-    # exec(open("algo.py").read())
     return {
         'msg': 'success',
     }
 
-
 @app.route('/getResults', methods=['GET'])
 def get_team_data():
-    cur = connection.cursor(buffered=True, dictionary=True)
-    query = 'select * from Team'
-    cur.execute(query)
-    data = cur.fetchall()
-    print(json.dumps(data))
-    return json.dumps(data)
-
+    data = connection.get_all_team_data()
+    return jsonify(data)
 
 @app.route('/Signup', methods=['GET', 'POST'])
 def signup():
-    name = str(request.form['name'])
-    hourly_rate = str(request.form['hourlyrate'])
-    dob = str(request.form['dob'])
-    languages = str(request.form['languages'])
-    is_assigned = str(0)
-    member_role = str(request.form['memberrole'])
-    experience = str(request.form['experience'])
-    skill_score = str(request.form['skillscore'])
-    available_hours_per_week = str(request.form['availablehoursperweek'])
-
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql = "INSERT INTO Member (MemberName,HourlyRate,DOB,Languages,IsAssigned,MemberRole,Experience,SkillScore,AvailableHoursPerWeek) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-        cursor.execute(sql, (
-            name, hourly_rate, dob, languages, is_assigned, member_role, experience, skill_score,
-            available_hours_per_week))
-        connection.commit()
+    data = request.form
+    connection.add_member(data)
     return redirect("http://localhost:3000/TeamFormationAssistant/Signup/Success")
 
 
 @app.route('/ProjectDetails', methods=['GET', 'POST'])
 def projectDetails():
-    name = str(request.form['name'])
-    end_date = str(request.form['enddate'])
-    team_size = str(request.form['teamsize'])
-    budget = str(request.form['budget'])
-    tools = str(request.form['tools'])
-    priority = str(request.form['priority'])
-    is_assignment_complete = str(0)
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql = "INSERT INTO Project (ProjectName,ProjectEndDate,ProjectTeamSize,Budget,Tools,Priority,IsAssignmentComplete) VALUES (%s,%s,%s,%s,%s,%s,%s);"
-        cursor.execute(sql, (name, end_date, team_size, budget, tools, priority, is_assignment_complete))
-        connection.commit()
+    data = request.form
+    connection.create_project(data)
+    if connection.Connection.is_connected():
         i = 0
 
         for key in request.form.items():
@@ -78,12 +48,12 @@ def projectDetails():
                 hours_weight = int(request.form['hoursweight'])
                 language_weight = int(request.form['languageweight'])
                 budget_weight = int(request.form['budgetweight'])
-                cursor = connection.cursor()
+                cursor = connection.Connection.cursor()
                 sql = "CALL populateRequirements (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
                 cursor.execute(sql, (
                     language_preferred, skill, member_role, available_hours_per_week, skill_weight, experience_weight,
                     hours_weight, language_weight, budget_weight))
-                connection.commit()
+                connection.Connection.commit()
                 i += 1
 
             else:
--- BackEnd/connection.py
+++ BackEnd/connection.py
@@ -1,11 +1,87 @@
-# MySql connection data present here
+import mysql.connector as conn
 
-import mysql.connector as mysql
+# Connect to Local MYSQL database on Dev team Desktops. FOr production Environments connect to AWS
+try:
+    Connection = conn.connect(
+        host="localhost",
+        user="root",
+        password="####",
+        database="teamformationassistant",
+        auth_plugin='mysql_native_password'
+    )
+    print("Connected to Developer Database")
+except:
+    Connection = conn.connect(
+        host="sefall2021.cosnmrdyk6wi.us-east-2.rds.amazonaws.com",
+        user="root",
+        password="SEFall2021",
+        database="teamformationassistant"
+    )
+    print("Switching to AWS DataBase")
 
-connection = mysql.connect(
-    host="localhost",
-    user="root",
-    password="####",
-    database="teamformationassistant",
-    auth_plugin='mysql_native_password'
-)
+def connect():
+    try:
+        Connection = conn.connect(
+            host="localhost",
+            user="root",
+            password="####",
+            database="teamformationassistant",
+            auth_plugin='mysql_native_password'
+        )
+    except:
+        Connection = conn.connect(
+            host="sefall2021.cosnmrdyk6wi.us-east-2.rds.amazonaws.com",
+            user="root",
+            password="SEFall2021",
+            database="teamformationassistant"
+        )
+
+def CheckConnection():
+    if not Connection.is_connected():
+        connect()
+
+def get_all_team_data():
+    '''Returns Team Assignment data for members who have already been assigned to a team'''
+    # ToDo: Create Modified Query to show further data of each member by joining with member table
+    # ToDo: To overcome Privacy Concers, show only the data of team members
+    CheckConnection()
+    with Connection.cursor(buffered=True, dictionary=True) as cursor:
+        cursor.execute('select * from Team;')
+        data = cursor.fetchall()
+    return data
+
+def add_member(data):
+    '''Adds the newly signed up user to the member Table'''
+    # ToDo: Get Project Details from user during signup. Current system assumes user has no project preference.
+    CheckConnection()
+    with Connection.cursor() as cursor:
+        name = str(data['name'])
+        hourly_rate = str(data['hourlyrate'])
+        dob = str(data['dob'])
+        languages = str(data['languages'])
+        is_assigned = str(0)
+        member_role = str(data['memberrole'])
+        experience = str(data['experience'])
+        skill_score = str(data['skillscore'])
+        available_hours_per_week = str(data['availablehoursperweek'])
+
+        query = "INSERT INTO Member (MemberName,HourlyRate,DOB,Languages,IsAssigned,MemberRole,Experience,SkillScore,AvailableHoursPerWeek) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+        cursor.execute(query, (name, hourly_rate, dob, languages, is_assigned, member_role, experience, skill_score,
+            available_hours_per_week))
+        Connection.commit()
+
+def create_project(data):
+    '''Adds new project details to project database'''
+    CheckConnection()
+    with Connection.cursor() as cursor:
+        name = str(data['name'])
+        end_date = str(data['enddate'])
+        team_size = str(data['teamsize'])
+        budget = str(data['budget'])
+        tools = str(data['tools'])
+        priority = str(data['priority'])
+        is_assignment_complete = str(0)
+
+        query = "INSERT INTO Project (ProjectName,ProjectEndDate,ProjectTeamSize,Budget,Tools,Priority,IsAssignmentComplete) VALUES (%s,%s,%s,%s,%s,%s,%s);"
+        cursor.execute(query, (name, end_date, team_size, budget, tools, priority, is_assignment_complete))
+        Connection.commit()
