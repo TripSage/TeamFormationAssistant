9e1058ac7778cfb7f21ba70e9d3be6796ed5e75d
Dhruvil Shah <dshah4@ncsu.edu>
2020-10-20 16:59:33 -0400

    Update Setup Instructions --- bloom.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='blossom',
+    version='0.0.1',
+    description='Like Phoenix one that blooms in adversity ',
+    author=['Dhruvil', 'Sreemoyee', 'Poorvaja', 'Neeraj', 'Sameer'],
+    author_email='dshah4@ncsu.edu',
+    packages=['BackEnd'],
+    license='Apache',
+    url='https://github.com/TripSage/TeamFormationAssistant',
+)
