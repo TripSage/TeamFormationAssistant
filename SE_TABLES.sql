135f3c09a66a68faf0801dd33de75989f6286da0
Poorvaja Penmetsa <ppenmet@ncsu.edu>
2020-10-22 18:23:22 -0700

    Update SE_TABLES.sql

--- SE_TABLES.sql
+++ SE_TABLES.sql
@@ -75,3 +75,62 @@ BEGIN
 END//   
 
 DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE reset()
+BEGIN
+
+		DROP TABLE IF EXISTS Member;
+        DROP TABLE IF EXISTS Project;
+        DROP TABLE IF EXISTS Team;
+        DROP TABLE IF EXISTS Requirements;
+        CREATE TABLE Member(
+			MemberId				INT NOT NULL AUTO_INCREMENT,
+			MemberName					VARCHAR(30) NOT NULL, 
+			DOB						DATE,
+			Languages				VARCHAR(100) NOT NULL,
+			IsAssigned				INT,
+			HourlyRate				FLOAT,
+			MemberRole				VARCHAR(200),
+			Experience				INT,
+			SkillScore				INT,
+			AvailableHoursPerWeek	INT,
+			PRIMARY KEY (MemberId)
+		);
+
+		CREATE TABLE Project(
+			ProjectId				INT NOT NULL AUTO_INCREMENT,
+			ProjectName					VARCHAR(30) NOT NULL,
+			ProjectEndDate					DATE,
+			ProjectTeamSize					INT,
+			Budget					FLOAT,
+			Tools					VARCHAR(100),
+			IsAssignmentComplete	INT,
+			Priority				INT,
+			PRIMARY KEY (ProjectId)
+		);
+
+		CREATE TABLE Team(
+			ProjectId	INT NOT NULL REFERENCES Member(MemberId),
+			ProjectName varchar(30),
+			MemberId	INT NOT NULL REFERENCES Project(ProjectId),
+				MemberName		VARCHAR(30) NOT NULL
+		);
+
+		CREATE TABLE Requirements(
+			JobId					INT NOT NULL AUTO_INCREMENT,
+			ProjectId				INT NOT NULL REFERENCES Project(ProjectId),
+			LanguagePreferred 				VARCHAR(20) NOT NULL,
+			Skill					INT NOT NULL,
+			MemberRole					VARCHAR(20),
+			AvailableHoursPerWeek	INT,
+			SkillWeight				INT,
+			ExperienceWeight		INT,
+			HoursWeight				INT,
+			LanguageWeight			INT,
+			BudgetWeight			INT,
+			PRIMARY KEY (JobId)
+		);
+END //
+DELIMITER ;
