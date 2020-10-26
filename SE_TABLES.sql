1ae52db7b9e560bc00a4cfbac7e677f6103957c4
Poorvaja Penmetsa <ppenmet@ncsu.edu>
2020-10-26 11:19:46 -0700

    Update SE_TABLES.sql

--- SE_TABLES.sql
+++ SE_TABLES.sql
@@ -7,12 +7,12 @@ DROP TABLE IF EXISTS Requirements;
 
 CREATE TABLE Member(
 	MemberId				INT NOT NULL AUTO_INCREMENT,
-	MemberName					VARCHAR(30) NOT NULL, 
+	MemberName					VARCHAR(1000) NOT NULL, 
 	DOB						DATE,
-	Languages				VARCHAR(100) NOT NULL,
+	Languages				VARCHAR(1000) NOT NULL,
 	IsAssigned				INT,
 	HourlyRate				FLOAT,
-	MemberRole				VARCHAR(200),
+	MemberRole				VARCHAR(1000),
 	Experience				INT,
 	SkillScore				INT,
 	AvailableHoursPerWeek	INT,
@@ -21,11 +21,11 @@ CREATE TABLE Member(
 
 CREATE TABLE Project(
 	ProjectId				INT NOT NULL AUTO_INCREMENT,
-	ProjectName					VARCHAR(30) NOT NULL,
+	ProjectName					VARCHAR(1000) NOT NULL,
 	ProjectEndDate					DATE,
 	ProjectTeamSize					INT,
 	Budget					FLOAT,
-	Tools					VARCHAR(100),
+	Tools					VARCHAR(1000),
 	IsAssignmentComplete	INT,
 	Priority				INT,
 	PRIMARY KEY (ProjectId)
@@ -33,17 +33,17 @@ CREATE TABLE Project(
 
 CREATE TABLE Team(
 	ProjectId	INT NOT NULL REFERENCES Member(MemberId),
-	ProjectName varchar(30),
+	ProjectName varchar(1000),
 	MemberId	INT NOT NULL REFERENCES Project(ProjectId),
-    	MemberName		VARCHAR(30) NOT NULL
+    	MemberName		VARCHAR(1000) NOT NULL
 );
 
 CREATE TABLE Requirements(
 	JobId					INT NOT NULL AUTO_INCREMENT,
 	ProjectId				INT NOT NULL REFERENCES Project(ProjectId),
-	LanguagePreferred 				VARCHAR(20) NOT NULL,
+	LanguagePreferred 				VARCHAR(1000) NOT NULL,
 	Skill					INT NOT NULL,
-	MemberRole					VARCHAR(20),
+	MemberRole					VARCHAR(1000),
 	AvailableHoursPerWeek	INT,
 	SkillWeight				INT,
 	ExperienceWeight		INT,
@@ -56,9 +56,9 @@ CREATE TABLE Requirements(
 
 DELIMITER //
 CREATE PROCEDURE populateRequirements(
-	IN vLanguagePreferred VARCHAR(255),
-    IN vSkill VARCHAR(255),
-    IN vMemberRole VARCHAR(255),
+	IN vLanguagePreferred VARCHAR(1000),
+    IN vSkill VARCHAR(1000),
+    IN vMemberRole VARCHAR(1000),
     IN vAvailableHoursPerWeek INT,
     IN vSkillWeight INT,
     IN vExperienceWeight INT,
@@ -87,12 +87,12 @@ BEGIN
         DROP TABLE IF EXISTS Requirements;
         CREATE TABLE Member(
 			MemberId				INT NOT NULL AUTO_INCREMENT,
-			MemberName					VARCHAR(30) NOT NULL, 
+			MemberName					VARCHAR(1000) NOT NULL, 
 			DOB						DATE,
-			Languages				VARCHAR(100) NOT NULL,
+			Languages				VARCHAR(1000) NOT NULL,
 			IsAssigned				INT,
 			HourlyRate				FLOAT,
-			MemberRole				VARCHAR(200),
+			MemberRole				VARCHAR(1000),
 			Experience				INT,
 			SkillScore				INT,
 			AvailableHoursPerWeek	INT,
@@ -101,11 +101,11 @@ BEGIN
 
 		CREATE TABLE Project(
 			ProjectId				INT NOT NULL AUTO_INCREMENT,
-			ProjectName					VARCHAR(30) NOT NULL,
+			ProjectName					VARCHAR(1000) NOT NULL,
 			ProjectEndDate					DATE,
 			ProjectTeamSize					INT,
 			Budget					FLOAT,
-			Tools					VARCHAR(100),
+			Tools					VARCHAR(1000),
 			IsAssignmentComplete	INT,
 			Priority				INT,
 			PRIMARY KEY (ProjectId)
@@ -113,17 +113,17 @@ BEGIN
 
 		CREATE TABLE Team(
 			ProjectId	INT NOT NULL REFERENCES Member(MemberId),
-			ProjectName varchar(30),
+			ProjectName varchar(1000),
 			MemberId	INT NOT NULL REFERENCES Project(ProjectId),
-				MemberName		VARCHAR(30) NOT NULL
+				MemberName		VARCHAR(1000) NOT NULL
 		);
 
 		CREATE TABLE Requirements(
 			JobId					INT NOT NULL AUTO_INCREMENT,
 			ProjectId				INT NOT NULL REFERENCES Project(ProjectId),
-			LanguagePreferred 				VARCHAR(20) NOT NULL,
+			LanguagePreferred 				VARCHAR(1000) NOT NULL,
 			Skill					INT NOT NULL,
-			MemberRole					VARCHAR(20),
+			MemberRole					VARCHAR(1000),
 			AvailableHoursPerWeek	INT,
 			SkillWeight				INT,
 			ExperienceWeight		INT,
