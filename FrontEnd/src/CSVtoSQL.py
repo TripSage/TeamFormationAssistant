1b255f79f6d56f213a80df39ffa1d1387d429cac
Dhruvil <dhruvilsshah@gmail.com>
2020-10-27 10:20:47 -0400

    Formatting with Black

--- FrontEnd/src/CSVtoSQL.py
+++ FrontEnd/src/CSVtoSQL.py
@@ -11,14 +11,16 @@ import sys
 
 connection = mysql.connector.connect(
     host="sefall2021.cosnmrdyk6wi.us-east-2.rds.amazonaws.com",
-    database='teamformationassistant',
+    database="teamformationassistant",
     user="root",
-    password="SEFall2021"
+    password="SEFall2021",
 )
 
+
 def main():
-    #verifyDatabaseConnection()
+    # verifyDatabaseConnection()
     loadEmployeeDataFromCSV()
+
 
 def verifyDatabaseConnection():
     if connection.is_connected():
@@ -32,35 +34,48 @@ def verifyDatabaseConnection():
         cursor.close()
         connection.close()
 
+
 def loadEmployeeDataFromCSV():
     EmployeeData = pd.read_csv("../DataSet/Employee.csv")
-    EmployeeData.columns = ['MemberID','Name','TeamID',
-                     'Languages','Role','HourlyRate','DOB','Experience',
-                     'SkillScore','Availibility','isAssigned']
+    EmployeeData.columns = [
+        "MemberID",
+        "Name",
+        "TeamID",
+        "Languages",
+        "Role",
+        "HourlyRate",
+        "DOB",
+        "Experience",
+        "SkillScore",
+        "Availibility",
+        "isAssigned",
+    ]
     if connection.is_connected():
         cursor = connection.cursor()
-        cursor.execute('DROP TABLE IF EXISTS Employee;')
-        cursor.execute('CREATE TABLE Employee(MemberID nvarchar(50), Name nvarchar(50), TeamID nvarchar(50), Languages nvarchar(50), Role nvarchar(50), HourlyRate nvarchar(50), DOB nvarchar(50), Experience nvarchar(50),SkillScore nvarchar(50),Availibility nvarchar(50),isAssigned nvarchar(50))')
+        cursor.execute("DROP TABLE IF EXISTS Employee;")
+        cursor.execute(
+            "CREATE TABLE Employee(MemberID nvarchar(50), Name nvarchar(50), TeamID nvarchar(50), Languages nvarchar(50), Role nvarchar(50), HourlyRate nvarchar(50), DOB nvarchar(50), Experience nvarchar(50),SkillScore nvarchar(50),Availibility nvarchar(50),isAssigned nvarchar(50))"
+        )
         for row in EmployeeData.index:
             sql = "INSERT INTO Employee(MemberID,Name,TeamID,Languages,Role,HourlyRate,DOB,Experience,SkillScore,Availibility,isAssigned)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-            cursor.execute(sql,(str(EmployeeData.loc[row, 'MemberID']),str(EmployeeData.loc[row, 'Name']),str(EmployeeData.loc[row, 'TeamID']),str(EmployeeData.loc[row, 'Languages']),
-            str(EmployeeData.loc[row,'Role']),str(EmployeeData.loc[row,'HourlyRate']),str(EmployeeData.loc[row, 'DOB']),
-            str(EmployeeData.loc[row, 'Experience']),str(EmployeeData.loc[row,'SkillScore']),str(EmployeeData.loc[row, 'Availibility']),str(EmployeeData.loc[row, 'isAssigned'])))
+            cursor.execute(
+                sql,
+                (
+                    str(EmployeeData.loc[row, "MemberID"]),
+                    str(EmployeeData.loc[row, "Name"]),
+                    str(EmployeeData.loc[row, "TeamID"]),
+                    str(EmployeeData.loc[row, "Languages"]),
+                    str(EmployeeData.loc[row, "Role"]),
+                    str(EmployeeData.loc[row, "HourlyRate"]),
+                    str(EmployeeData.loc[row, "DOB"]),
+                    str(EmployeeData.loc[row, "Experience"]),
+                    str(EmployeeData.loc[row, "SkillScore"]),
+                    str(EmployeeData.loc[row, "Availibility"]),
+                    str(EmployeeData.loc[row, "isAssigned"]),
+                ),
+            )
         connection.commit()
 
-if __name__=="__main__": 
-    main() 
 
-
-
-  
-         
-        
-
-
-
-    
-    
-
-
-
+if __name__ == "__main__":
+    main()
