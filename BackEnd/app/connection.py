b1a98c5eccbf5fd83ad10131d3f63b7f74ba12f6
Dhruvil <dhruvilsshah@gmail.com>
2020-10-27 10:18:39 -0400

    Formatting with Black

--- BackEnd/app/connection.py
+++ BackEnd/app/connection.py
@@ -7,7 +7,7 @@ try:
         user="root",
         password="DatabasePass@54",
         database="teamformationassistant",
-        auth_plugin='mysql_native_password'
+        auth_plugin="mysql_native_password",
     )
     print("Connected to Developer Database")
 except:
@@ -15,7 +15,7 @@ except:
         host="sefall2021.cosnmrdyk6wi.us-east-2.rds.amazonaws.com",
         user="root",
         password="SEFall2021",
-        database="teamformationassistant"
+        database="teamformationassistant",
     )
     print("Switching to AWS DataBase")
 
@@ -27,14 +27,14 @@ def connect():
             user="root",
             password="DatabasePass@54",
             database="teamformationassistant",
-            auth_plugin='mysql_native_password'
+            auth_plugin="mysql_native_password",
         )
     except:
         Connection = conn.connect(
             host="sefall2021.cosnmrdyk6wi.us-east-2.rds.amazonaws.com",
             user="root",
             password="SEFall2021",
-            database="teamformationassistant"
+            database="teamformationassistant",
         )
 
 
@@ -49,7 +49,7 @@ def get_all_team_data():
     # ToDo: To overcome Privacy Concerns, show only the data of team members
     CheckConnection()
     with Connection.cursor(buffered=True, dictionary=True) as cursor:
-        cursor.execute('select * from Team;')
+        cursor.execute("select * from Team;")
         data = cursor.fetchall()
     return data
 
@@ -60,92 +60,129 @@ def get_all_member_data():
     # ToDo: To overcome Privacy Concerns, show only the data of team members
     CheckConnection()
     with Connection.cursor(buffered=True, dictionary=True) as cursor:
-        cursor.execute('select * from Member;')
+        cursor.execute("select * from Member;")
         data = cursor.fetchall()
     return data
 
 
 def add_member(data):
-    '''Adds the newly signed up user to the member Table'''
+    """Adds the newly signed up user to the member Table"""
     # ToDo: Get Project Details from user during signup. Current system assumes user has no project preference.
 
     try:
         CheckConnection()
         with Connection.cursor() as cursor:
-            name = str(data['name'])
-            hourly_rate = str(data['hourlyrate'])
-            dob = str(data['dob'])
-            languages = str(data['languages'])
+            name = str(data["name"])
+            hourly_rate = str(data["hourlyrate"])
+            dob = str(data["dob"])
+            languages = str(data["languages"])
             is_assigned = str(0)
-            member_role = str(data['memberrole'])
-            experience = str(data['experience'])
-            skill_score = str(data['skillscore'])
-            available_hours_per_week = str(data['availablehoursperweek'])
+            member_role = str(data["memberrole"])
+            experience = str(data["experience"])
+            skill_score = str(data["skillscore"])
+            available_hours_per_week = str(data["availablehoursperweek"])
 
             query = "INSERT INTO Member (MemberName,HourlyRate,DOB,Languages,IsAssigned,MemberRole,Experience,SkillScore,AvailableHoursPerWeek) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-            cursor.execute(query, (name, hourly_rate, dob, languages, is_assigned, member_role, experience, skill_score,
-                                available_hours_per_week))
+            cursor.execute(
+                query,
+                (
+                    name,
+                    hourly_rate,
+                    dob,
+                    languages,
+                    is_assigned,
+                    member_role,
+                    experience,
+                    skill_score,
+                    available_hours_per_week,
+                ),
+            )
             Connection.commit()
             return True
 
-    except conn.Error as error :
+    except conn.Error as error:
         print("Failed to update record to database rollback: {}".format(error))
         Connection.rollback()
         return False
 
 
 def create_project(data):
-    '''Adds new project details to project database'''
+    """Adds new project details to project database"""
     try:
         CheckConnection()
         with Connection.cursor() as cursor:
-            name = str(data['name'])
-            end_date = str(data['enddate'])
-            team_size = str(data['teamsize'])
-            budget = str(data['budget'])
-            tools = str(data['tools'])
-            priority = str(data['priority'])
+            name = str(data["name"])
+            end_date = str(data["enddate"])
+            team_size = str(data["teamsize"])
+            budget = str(data["budget"])
+            tools = str(data["tools"])
+            priority = str(data["priority"])
             is_assignment_complete = str(0)
 
             query = "INSERT INTO Project (ProjectName,ProjectEndDate,ProjectTeamSize,Budget,Tools,Priority,IsAssignmentComplete) VALUES (%s,%s,%s,%s,%s,%s,%s);"
-            cursor.execute(query, (name, end_date, team_size, budget, tools, priority, is_assignment_complete))
+            cursor.execute(
+                query,
+                (
+                    name,
+                    end_date,
+                    team_size,
+                    budget,
+                    tools,
+                    priority,
+                    is_assignment_complete,
+                ),
+            )
             Connection.commit()
         return True
-    
-    except conn.Error as error :
+
+    except conn.Error as error:
         print("Failed to update record to database rollback: {}".format(error))
         Connection.rollback()
         return False
+
 
 def save_project_requirements(data):
     try:
         CheckConnection()
         with Connection.cursor() as cursor:
-            i = 0 
+            i = 0
             for key in data.items():
-                colname = 'languagepreferred' + str(i)
+                colname = "languagepreferred" + str(i)
                 if colname in data:
                     language_preferred = str(data[colname])
-                    skill = str(data['skill' + str(i)])
-                    member_role = str(data['memberrole' + str(i)])
-                    available_hours_per_week = int(data['availablehoursperweek' + str(i)])
-                    skill_weight = int(data['skillweight'])
-                    experience_weight = int(data['experienceweight'])
-                    hours_weight = int(data['hoursweight'])
-                    language_weight = int(data['languageweight'])
-                    budget_weight = int(data['budgetweight'])
-                
+                    skill = str(data["skill" + str(i)])
+                    member_role = str(data["memberrole" + str(i)])
+                    available_hours_per_week = int(
+                        data["availablehoursperweek" + str(i)]
+                    )
+                    skill_weight = int(data["skillweight"])
+                    experience_weight = int(data["experienceweight"])
+                    hours_weight = int(data["hoursweight"])
+                    language_weight = int(data["languageweight"])
+                    budget_weight = int(data["budgetweight"])
+
                     sql = "CALL populateRequirements (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                    cursor.execute(sql, (
-                        language_preferred, skill, member_role, available_hours_per_week, skill_weight, experience_weight,
-                        hours_weight, language_weight, budget_weight))
+                    cursor.execute(
+                        sql,
+                        (
+                            language_preferred,
+                            skill,
+                            member_role,
+                            available_hours_per_week,
+                            skill_weight,
+                            experience_weight,
+                            hours_weight,
+                            language_weight,
+                            budget_weight,
+                        ),
+                    )
                     Connection.commit()
                     i += 1
                 else:
                     break
         return True
 
-    except conn.Error as error :
+    except conn.Error as error:
         print("Failed to update record to database rollback: {}".format(error))
         Connection.rollback()
         return False
--- BackEnd/app/routes.py
+++ BackEnd/app/routes.py
@@ -11,36 +11,41 @@ from app import app
 # app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/executeAlgo')
+
+@app.route("/executeAlgo")
 def execute_algo():
     exec(open("TeamAssigner.py").read())
     return {
-        'msg': 'success',
+        "msg": "success",
     }
 
-@app.route('/getResults', methods=['GET'])
+
+@app.route("/getResults", methods=["GET"])
 def get_team_data():
     data = connection.get_all_team_data()
     return jsonify(data)
 
-@app.route('/getMembers', methods=['GET'])
+
+@app.route("/getMembers", methods=["GET"])
 def get_member_data():
     data = connection.get_all_member_data()
     return jsonify(data)
 
-@app.route('/Signup', methods=['GET', 'POST'])
+
+@app.route("/Signup", methods=["GET", "POST"])
 def signup():
     data = request.form
     if connection.add_member(data) == True:
         return redirect("http://localhost:3000/TeamFormationAssistant/Signup/Success")
 
 
-@app.route('/ProjectDetails', methods=['GET', 'POST'])
+@app.route("/ProjectDetails", methods=["GET", "POST"])
 def projectDetails():
     data = data
     result = save_project_details(data)
     if result == True:
-        return redirect("http://localhost:3000/TeamFormationAssistant/Signup/Success");
+        return redirect("http://localhost:3000/TeamFormationAssistant/Signup/Success")
+
 
 def save_project_details(data):
     connection.create_project(data)
